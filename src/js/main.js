982cbf5285b941f1781406df7a960e01f5aa28f6
rocio_ld <rocio_ld@hotmail.com>
2024-08-08 12:43:56 +0200

    js funcion nombre

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+//llamar al input
+
+const nameInput = document.querySelector ('.js-name');
+const title= document.querySelector ('.js-title');
+//escuchar el evento relacionandola con la funcion,declarar la funcion handleInputName y hallar el valor , y para pintarlo en la tarjeta buscar el title en html y añadirla con innerHTML.
+
+function handleInputName () {
+    const valueName = nameInput.value;
+    title.innerHTML = valueName;
+}
+
+
+nameInput.addEventListener ('input', handleInputName);
