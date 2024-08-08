59171d03c2c25610b0608055c90cc93be91b8fc7
Anka <ankalucian@yahoo.es>
2024-08-08 13:20:20 +0200

    cambios formulario sexo perro

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,8 @@ const nameInput = document.querySelector ('.js-name');
 const title= document.querySelector ('.js-title');
 const ageInput = document.querySelector ('.js-age');
 const ageCard = document.querySelector ('.js-age-card');
+const sexInput =document.querySelector ('.js-sexo');
+const sexCard = document.querySelector ('.js-sex-card');
 //escuchar el evento relacionandola con la funcion,declarar la funcion handleInputName y hallar el valor , y para pintarlo en la tarjeta buscar el title en html y añadirla con innerHTML.
 
 function handleInputName () {
@@ -21,3 +23,9 @@ function handleInputAge () {
     ageCard.innerHTML = valueAge + '' + `años`;
 }
 ageInput.addEventListener ('input', handleInputAge);
+
+function handleInputSex () {
+    const valueSex =sexInput.value;
+    sexCard.innerHTML=valueSex;
+}
+sexInput.addEventListener ('input', handleInputSex);
