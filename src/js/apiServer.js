d2291c6207d0985a95db6d2029f3beafefad3de9
Shannnon Evora Lopes Rodrigues <shannonelr9208@icloud.com>
2024-09-03 09:41:39 +0200

    final

--- src/js/apiServer.js
+++ src/js/apiServer.js
@@ -8,7 +8,7 @@ const handleCreateCard=(ev)=>{
         headers: {
             'Content-Type': 'application/json',
         },
-    });
+    })
     .then((response)=>response.json())
     .then((data)=>{
          console.log(data);
